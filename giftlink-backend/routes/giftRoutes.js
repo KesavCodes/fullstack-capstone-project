9d49fea9257f5c5c19d20edf74bab25359a39141
KesavCodes <kesavakumarcode@outlook.com>
2024-06-21 13:41:38 -0400

    changes with sol

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,13 +1,17 @@
-router.get('/', async (req, res) => {
-    try {t
-        const db = await connectToDatabase();
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
 
+
+router.get('/', async (req, res) => {
+    logger.info('/ called');
+    try {
+        const db = await connectToDatabase();
         // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
         const gifts = await collection.find({}).toArray();
-
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
     } catch (e) {
@@ -20,19 +24,14 @@ router.get('/:id', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-
         // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection('gifts');
-
         const id = req.params.id;
-
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        const gift = await collection.findOne({id: id})
-
+        const gift = await collection.findOne({id: id});
         if (!gift) {
             return res.status(404).send('Gift not found');
         }
-
         res.json(gift);
     } catch (e) {
         console.error('Error fetching gift:', e);
